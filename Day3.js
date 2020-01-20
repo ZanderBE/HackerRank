1147bd5a4cd1f1eecc78541c4f065ee86749ddb2
Zander <zanderbrownlieedward@gmail.com>
2020-01-20 13:59:01 -0700

    Adding Day 3 solution to the repo.

--- Day3.js
+++ Day3.js
@@ -1,2 +1,15 @@
-// Day 2 Problem for the 30 day HackerRank Code Challenge
-// Operators
+// Day 3 Problem for the 30 day HackerRank Code Challenge
+// Intro to Conditional Statements
+function main() {
+    const N = parseInt(readLine(), 10);
+    if (N % 2 === 1) {
+        console.log('Weird');
+    } else if(N % 2 === 0 && N >= 2 && N <= 5) {
+        console.log('Not Weird');
+    } else if(N % 2 === 0 && N >= 6 && N <= 20) {
+        console.log('Weird');
+    } else if(N % 2 === 0 && N >= 20) {
+        console.log('Not Weird');
+    }
+}
+
