0c146d2283f86dca4831ca753f843d56bc5d945b
Zander <zanderbrownlieedward@gmail.com>
2020-01-21 22:14:46 -0700

    Adding Day 5 solution to the file.

--- Day5.js
+++ Day5.js
@@ -1,2 +1,9 @@
 // Day 5 Problem for the 30 day HackerRank Code Challenge
 // Loops
+function main() {
+    const n = parseInt(readLine(), 10);
+    for(let i=1; i <= 10; i++) {
+        let result = n * i;
+        console.log(n + " x " + i + " = " + result);
+    }
+}
