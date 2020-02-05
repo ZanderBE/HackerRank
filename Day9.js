94c4ad8cc0fe8f8b4e605b0c274b0cafbda71b3a
Zander <zanderbrownlieedward@gmail.com>
2020-02-05 09:59:57 -0700

    Adding solution to the repo.

--- Day9.js
+++ Day9.js
@@ -1,2 +1,12 @@
 // Day 9 Problem for the 30 day HackerRank Code Challenge
 // Recursion 3
+// Complete the factorial function below.
+function factorial(n) {
+    // If the number equals 0, return the value of 1
+    if(n === 0) {
+        return 1;
+    // Anything else, take the number and multiply it by the factorial of n minus one.
+    } else {
+        return n * factorial(n-1);
+    }
+}
