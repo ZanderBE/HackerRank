8e8c55f74cc97e35ac3b5756cd0516b2fff5bd01
Zander <zanderbrownlieedward@gmail.com>
2020-02-19 10:32:37 -0700

    Adding Day16 solution to the repo.

--- Day16.js
+++ Day16.js
@@ -1,3 +1,13 @@
 // Day 16 Problem for the 30 day HackerRank Code Challenge
 // Exceptions - String to Integer
-
+function main() {
+    let S = readLine();
+    S = parseInt(S);
+    try {
+        // this is a hacky way of doing this in javascript since it doesn't handle exceptions well. (not a hard typed lang)
+        new Array(S);
+        console.log(S);
+    } catch(err) {
+        console.log('Bad String');
+    }
+}
