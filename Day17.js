595da4c5528bd4c723c5bc2ec769306a315a41f9
Zander <zanderbrownlieedward@gmail.com>
2020-02-19 12:08:27 -0700

    Adding Day17 solution to the repo.

--- Day17.js
+++ Day17.js
@@ -1,2 +1,15 @@
 // Day 17 Problem for the 30 day HackerRank Code Challenge
 // Exceptions - String to Integer
+// Write your code here
+class Calculator {
+    // Power Method 
+    power(n, p) {
+         if (n < 0 || p < 0) {
+          throw 'n and p should be non-negative';
+         } else {
+          let answer = n**p;
+          return answer;
+         }
+     } 
+ }
+ 
