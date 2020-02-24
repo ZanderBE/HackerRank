d44e3c4d7169626b15a6edf4adfb45e04c374757
Zander <zanderbrownlieedward@gmail.com>
2020-02-24 16:44:38 -0700

    Adding Day18 solution to the repo.

--- Day18.js
+++ Day18.js
@@ -1,2 +1,29 @@
 // Day 18 Problem for the 30 day HackerRank Code Challenge
 // Queues and Stacks
+function Solution(){
+    //Write your code here
+    // A: Write two instance variables for stack & queue
+    let stack = [];
+    let queue = [];
+    
+  
+    // B: Method pushes character onto a stack
+    this.pushCharacter = ((ch) => {
+       return stack.push(ch);
+    });
+  
+    // C: Method enques CH in the queue instance variable
+    this.enqueueCharacter = ((ch) => {
+        return queue.unshift(ch);
+    });
+  
+    // D: Method pops and returns CH at the top of stack      instance variable
+    this.popCharacter = (() => {
+        return stack.shift();
+    });
+  
+    // E: Method dequeue and return first CH in queue instance variable
+    this.dequeueCharacter = (() => {
+        return queue.shift();
+    }); 
+  }
