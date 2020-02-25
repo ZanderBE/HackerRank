87c14628ab2a34dcbd03574a62c32b097222660d
ZanderBE <zanderbrownlieedward@gmail.com>
2020-02-25 01:07:28 -0700

    Add Day14 solution to the repo.

--- Day14.js
+++ Day14.js
@@ -1,3 +1,12 @@
 // Day 14 Problem for the 30 day HackerRank Code Challenge
 // Scope
-// Javascript is not available on this IDE. I'll circle back to this one later. 
+// C# solution as JS was not an option
+public Difference(int[] a )
+{
+elements = a;
+}
+public void computeDifference(){
+int max = elements.Max();
+int min = elements.Min();
+maximumDifference = max - min;
+}
