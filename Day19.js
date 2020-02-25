f102c81a7f27ffebb63ebe7a28d6450d6e90dd45
ZanderBE <zanderbrownlieedward@gmail.com>
2020-02-25 01:01:17 -0700

    Add Day19 solution to the repo.

--- Day19.js
+++ Day19.js
@@ -1,2 +1,12 @@
 // Day 19 Problem for the 30 day HackerRank Code Challenge
 // Interfaces
+// C# solution as JS was not an option
+public class Calculator : AdvancedArithmetic {
+    public int divisorSum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) sum += i;
+        }
+        return sum;
+    }
+}
